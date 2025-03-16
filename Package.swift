d353d774e64e083a89a0ab544730da99ff76091d
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-03-16 22:03:52 +0900

    基礎編向け

--- Package.swift
+++ Package.swift
@@ -3,42 +3,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "LineCountPlugin",
+    name: "LineCounter",
     products: [
-        .plugin(
-            name: "LineCountPlugin",
-            targets: ["LineCountPlugin"]
-        ),
-        .library(
-            name: "Dummy",
-            targets: ["Dummy"]
+        .executable(
+            name: "lc",
+            targets: ["LineCounter"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.5.0"),
-    ],
     targets: [
-        .target(name: "LineCounterCore"),
-        .executableTarget(
-            name: "LineCounter",
-            dependencies: [
-                "LineCounterCore",
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-        .testTarget(
-            name: "LineCounterCoreTests",
-            dependencies: ["LineCounterCore"],
-            resources: [.process("Resources")]
-        ),
-        .plugin(
-            name: "LineCountPlugin",
-            capability: .buildTool(),
-            dependencies: ["LineCounter"]
-        ),
-        .target(
-            name: "Dummy",
-            plugins: ["LineCountPlugin"]
-        ),
+        .executableTarget(name: "LineCounter")
     ]
 )
--- Sources/LineCounter/main.swift
+++ Sources/LineCounter/main.swift
@@ -1,21 +1,14 @@
-import ArgumentParser
 import Foundation
-import LineCounterCore
 
-struct LC: ParsableCommand {
-    static let configuration = CommandConfiguration(
-        commandName: "lc",
-        abstract: "A tool to count the number of lines in the specified file.",
-        version: "0.0.1"
-    )
-
-    @Argument(help: "The absolute path of a file.")
-    var path: String
-
-    mutating func run() throws {
-        let count = try LineCounterCore(path: path).count()
-        print("\(count)\t\(path)")
-    }
+guard CommandLine.arguments.count == 2 else {
+    print("usage: lc [absolute path of a file]")
+    exit(1)
 }
 
-LC.main()
+let fileURL = URL(fileURLWithPath: CommandLine.arguments[1])
+guard let file = try? String(contentsOf: fileURL, encoding: .utf8) else {
+    print("Error: could not read \(fileURL.absoluteString)")
+    exit(2)
+}
+
+print(file.components(separatedBy: CharacterSet.newlines).count)
